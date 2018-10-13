e3e6e5360de1f71cd56261211eb682022d054433
shazwi suwandi <supershazwi@gmail.com>
2018-10-14 01:46:54 +0800

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,7 +67,7 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'talentail-217309'),
-            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', 'https://00e9e64bac6dfb0a4a2ea3a57695cbf973195843b61b81f0c4-apidata.googleusercontent.com/download/storage/v1/b/talentail-123456789/o/talentail-217309-ac6e4e86e4f1.json?qk=AD5uMEsXP3FbushFXw0_ddAAX503mLYDH7puF2WrHOV_yfUYWdIKIZKaZjtacHHVN77Bc42ylcK_Dvq_L1PhBkwKIA9SF7gN6TD6COXyjFUKbZzOfOCCfl57VYKEZYpcHRo95BnKRGvsjYh-MQ79Pp8CsJcpTqL7Kzo8dgWMgav7DGAbDWKvarRW8nyHqL5vGNCvjp4qUVNizhCIR9BPsrCZF0ks01ufYNXxeYwm6cr8B6UmSF8HAF9jX9RaPqxv31R4dGnis5M-BhizTS_wYoPsZrSxmbpH137onEMx1Nfqc9ZNUwsrPyiV-T3Lntfe4NIKuqVWm1WIamZAAdmVcZnnvawj5PtGuVf_llMXxhbcDbkwKN_z5kaSDMhFpbOLlWAc7yiwTHRN_QZZUuokbflEJOQxkmsWUkjteduRRi9Q7YwMSh9lsTI83wALtgRzQYNde6C4a7I3gNs3Iv_H6u7UTSOs1y8IM9JW0ZEgVpWMthafLUc5mA8BXzq5NQx4vgF_YqILAxMQ-m6RNKs5z8rfI2deewzJ5f-4n5BzVgC1SSLKWaWHNjZUJ2SKqsKu5Yzg8s_jqcoeiLKwMGz4C95zaXVs_UM_pyRPVgEHTIEpwjJ4l_5cLlWsHSSq03g7qiYXc59tFDG2MranUwxQTW8vMaDZ0mNtD83x7P-dQgoXivRMrYbH28FIjJOlvwlBJ04dmE6lu9r3iRV_L-eqjCeukkAA2zw317SyPo_WhGMlbCvMJ2hF2x_rzlg1_yMfRUVEAtvNVCfPjgQVzLsYnS5OKcWX9KUQUaylHwpx9SUMHE78wMWOB8g'), // optional: /path/to/service-account.json
+            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', '/var/www/html/talentail/current/talentail-217309-ac6e4e86e4f1.json'), // optional: /path/to/service-account.json
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'talentail-123456789'),
             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
