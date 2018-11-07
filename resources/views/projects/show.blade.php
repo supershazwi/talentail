68cf4ddb5cdb55300391db0bff9c7183b6eb6328
shazwi suwandi <supershazwi@gmail.com>
2018-11-07 13:39:35 +0800

    update

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -489,27 +489,6 @@
       document.getElementById("rectangleChat").style.borderColor = "#076bff";
     }
 
-    var button = document.querySelector('#submit-button');
-
-    // braintree.dropin.create({
-    //   authorization: "{{ Braintree_ClientToken::generate() }}",
-    //   container: '#dropin-container'
-    // }, function (createErr, instance) {
-    //   button.addEventListener('click', function () {
-    //     instance.requestPaymentMethod(function (err, payload) {
-
-    //       $.get('{{ route('payment.process') }}', {payload}, function (response) {
-    //         if (response.success) {
-    //           alert('Payment successfull!');
-    //         } else {
-    //           alert('Payment failed');
-    //         }
-    //       }, 'json');
-
-    //     });
-    //   });
-    // });
-
   </script>
 
   <script type="text/javascript">
