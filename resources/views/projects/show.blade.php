b9c4eb8d3dd15d475812834afaeb4922939567fb
shazwi suwandi <supershazwi@gmail.com>
2018-11-07 13:38:16 +0800

    updatE

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -491,24 +491,24 @@
 
     var button = document.querySelector('#submit-button');
 
-    braintree.dropin.create({
-      authorization: "{{ Braintree_ClientToken::generate() }}",
-      container: '#dropin-container'
-    }, function (createErr, instance) {
-      button.addEventListener('click', function () {
-        instance.requestPaymentMethod(function (err, payload) {
+    // braintree.dropin.create({
+    //   authorization: "{{ Braintree_ClientToken::generate() }}",
+    //   container: '#dropin-container'
+    // }, function (createErr, instance) {
+    //   button.addEventListener('click', function () {
+    //     instance.requestPaymentMethod(function (err, payload) {
 
-          $.get('{{ route('payment.process') }}', {payload}, function (response) {
-            if (response.success) {
-              alert('Payment successfull!');
-            } else {
-              alert('Payment failed');
-            }
-          }, 'json');
+    //       $.get('{{ route('payment.process') }}', {payload}, function (response) {
+    //         if (response.success) {
+    //           alert('Payment successfull!');
+    //         } else {
+    //           alert('Payment failed');
+    //         }
+    //       }, 'json');
 
-        });
-      });
-    });
+    //     });
+    //   });
+    // });
 
   </script>
 
