04582a06dbf21b61b7705b172b7ca4e031c528bb
shazwi suwandi <supershazwi@gmail.com>
2018-12-01 02:45:25 +0800

    update

--- database/migrations/2018_11_30_110156_add_thumbnailurl_to_projects_table.php
+++ database/migrations/2018_11_30_110156_add_thumbnailurl_to_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddThumbnailToProjectsTable extends Migration
+class AddThumbnailurlToProjectsTable extends Migration
 {
     /**
      * Run the migrations.
