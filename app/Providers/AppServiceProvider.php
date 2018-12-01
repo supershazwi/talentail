40bf380b582ccbf47c0539fbe05457c2e60c6f7b
shazwi suwandi <supershazwi@gmail.com>
2018-12-01 11:20:32 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,10 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        \Braintree_Configuration::environment(env('BRAINTREE_ENV'));
-        \Braintree_Configuration::merchantId(env('BRAINTREE_MERCHANT_ID'));
-        \Braintree_Configuration::publicKey(env('BRAINTREE_PUBLIC_KEY'));
-        \Braintree_Configuration::privateKey(env('BRAINTREE_PRIVATE_KEY'));
+        Braintree_Configuration::environment(env('BRAINTREE_ENV'));
+        Braintree_Configuration::merchantId(env('BRAINTREE_MERCHANT_ID'));
+        Braintree_Configuration::publicKey(env('BRAINTREE_PUBLIC_KEY'));
+        Braintree_Configuration::privateKey(env('BRAINTREE_PRIVATE_KEY'));
 
         $pusher = $this->app->make('pusher');
         $pusher->set_logger( new LaravelLoggerProxy() );
