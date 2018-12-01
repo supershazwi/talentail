16f7d74060c466b3fe061b35a9ed9b5b420b8918
shazwi suwandi <supershazwi@gmail.com>
2018-12-02 05:44:58 +0800

    update

--- resources/views/shoppingCart.blade.php
+++ resources/views/shoppingCart.blade.php
@@ -469,6 +469,9 @@
       <button type="submit" style="display: none;" id="processPaymentButton">Submit</button>
     </form>
 
+
+    <!-- -->
+
     <div class="modal fade" id="modalMembers" tabindex="-1" role="dialog" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
